3010a1d93d6b54b1a4007a8c22a24d2c6ec188fb
JoshuaParungao <jmphotography1830@gmail.com>
2025-11-04 11:19:11 +0800

    baseURL Updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/posts", 
+  baseURL: "https://pcit9-frontend.onrender.com/api/posts", 
 });
 
 export default API;
